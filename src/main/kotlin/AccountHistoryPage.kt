3db56381ed07c3e9cf5f0148fa21a2f77a1caab3
Szymon Filipczyk <soomalx@gmail.com>
2019-07-11 01:58:05 +0200

    #8 split transactions by day

--- src/main/kotlin/AccountHistoryPage.kt
+++ src/main/kotlin/AccountHistoryPage.kt
@@ -1,12 +1,16 @@
+import org.w3c.dom.Element
 import org.w3c.dom.asList
+import transactions.TransactionPresenter
 import transactions.Transaction
 import transactions.TransactionStore
+import transactions.decorators.SplitByDaysDecorator
 import kotlin.browser.document
 import kotlin.math.abs
 import kotlin.math.log
 
 class AccountHistoryPage {
     private val domTransactions = document.getElementsByClassName("content-list-row").asList()
+
     private val transactionStore: TransactionStore = TransactionStore()
 
     init {
@@ -24,9 +28,14 @@ class AccountHistoryPage {
     }
 
     private fun redecorateDomTransactions() {
-        transactionStore.replaceAll(domTransactions.map { Transaction.from(it) })
+        val transactionPresenters = buildTransactionPresentersFrom(domTransactions)
+        transactionStore.replaceAll(transactionPresenters.map { Transaction.from(it) })
+
         decorateTransactions(transactionStore.findAll())
     }
+
+    private fun buildTransactionPresentersFrom(domTransactions: List<Element>) =
+        domTransactions.mapIndexed { index, element -> TransactionPresenter(element, index) }
 
     private fun hasNewDomTransactionsBeenLoaded(): Boolean {
         return domTransactions.count() > transactionStore.getTransactionsCount()
@@ -35,26 +44,28 @@ class AccountHistoryPage {
     private fun decorateTransactions(transactions: List<Transaction>) {
         var greatestTransactionAmount = transactionStore.getGreatestTransactionAmount()
 
-        if(greatestTransactionAmount!! > 1000) {
+        if (greatestTransactionAmount!! > 1000) {
             greatestTransactionAmount = 1000.0
         }
 
         for (it in transactions.iterator()) {
             if (it.amount!! < 0) {
                 val relativeAmount = abs(it.amount!! / greatestTransactionAmount)
-                it.domTransaction?.setAttribute(
+                it.transactionPresenter?.setAttribute(
                     "style",
                     "background: " + getBackgroundColorForRelativeAmount(relativeAmount)
                 )
             }
         }
+
+        SplitByDaysDecorator().decorate(transactions)
     }
 
     private fun getBackgroundColorForRelativeAmount(relativeAmount: Double): String {
         val maxHue = 120
         val logLimit = 8
         val logScale = maxHue / logLimit
-        val hue = maxHue - logScale * log(100*relativeAmount, 2.0)
+        val hue = maxHue - logScale * log(100 * relativeAmount, 2.0)
 
         return "hsl($hue,100%,90%)"
     }
---
+++ src/main/kotlin/transactions/TransactionPresenter.kt
@@ -0,0 +1,17 @@
+package transactions
+
+import org.w3c.dom.Element
+
+class TransactionPresenter(var element: Element, val index: Int) {
+    fun getAttribute(name: String): String? {
+        return element.getAttribute(name)
+    }
+
+    fun setAttribute(key: String, value: String) {
+        element.setAttribute(key, value)
+    }
+
+    fun getDateFromTextField(): String {
+        return element.querySelector(".date")!!.textContent!!
+    }
+}
