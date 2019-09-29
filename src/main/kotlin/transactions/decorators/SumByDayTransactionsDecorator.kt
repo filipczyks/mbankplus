e6ed8afcf27dad962827ee6103c9b71756538149
Szymon Filipczyk <soomalx@gmail.com>
2019-09-29 21:16:09 +0200

    fix: prepend summary, don't replace first element

--- src/main/kotlin/transactions/decorators/SumByDayTransactionsDecorator.kt
+++ src/main/kotlin/transactions/decorators/SumByDayTransactionsDecorator.kt
@@ -1,8 +1,12 @@
 package transactions.decorators
 
 import org.w3c.dom.Element
+import org.w3c.dom.HTMLElement
+import org.w3c.dom.Node
 import transactions.Transaction
 import utils.DateUtils
+import kotlin.browser.document
+import kotlin.dom.createElement
 import kotlin.js.Date
 
 class SumByDayTransactionsDecorator : TransactionsDecorator {
@@ -16,14 +20,16 @@ class SumByDayTransactionsDecorator : TransactionsDecorator {
 
                 val expensesSum = it.filter { it.amount!! < 0 }
                     .sumByDouble { it.amount!! }
-                insertSummary(firstRow, it[0].createdAt!!, expensesSum)
+                
+                prependSummary(firstRow, it[0].createdAt!!, expensesSum)
             }
     }
 
-    private fun insertSummary(element: Element, date: Date, expensesSum: Double) {
+    private fun prependSummary(element: Element, date: Date, expensesSum: Double) {
         val dateString = "${DateUtils.pad(date.getDate())}.${DateUtils.pad(date.getMonth())}.${date.getFullYear()}"
 
-        element.innerHTML =
+        val summary = document.createElement("li")
+        summary.innerHTML =
                 "<li class=\"content-list-row collapsed sum\" data-category-id=\"14\" data-currency=\"PLN\">\n" +
                 "    <header class=\"content-list-row-header\">\n" +
                 "        <div class=\"column type\">\n" +
@@ -43,8 +49,7 @@ class SumByDayTransactionsDecorator : TransactionsDecorator {
                 "            </div>\n" +
                 "        </div>\n" +
                 "        <div class=\"column actions\">\n" +
-                "            <ul>\n" +
-                "                            </ul>\n" +
+                "            <ul>\n</ul>\n" +
                 "        </div>\n" +
                 "        <div class=\"column amount\">\n" +
                 "            <strong class=\"negative\">\n" +
@@ -54,5 +59,7 @@ class SumByDayTransactionsDecorator : TransactionsDecorator {
                 "    </header>\n" +
                 "\n" +
                 "</li>"
+
+        element.parentNode?.insertBefore(summary, element)
     }
 }
